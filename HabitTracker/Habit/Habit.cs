c548803671685cd8e47461f6bd2278a32ddd25ca
KahlanR687 <kahlanroberts687@gmail.com>
2026-06-26 19:40:09 -0600

    Create new HabitTracker solution, Create Habit Class and add basic methods and comments to Habit.cs

--- HabitTracker/Habit/Habit.cs
+++ HabitTracker/Habit/Habit.cs
@@ -1,11 +1,85 @@
-﻿
-class Habit
+﻿namespace Habit;
+
+/// <summary>
+///     <para>
+///         This class is a model for a singular habit.
+///         It represents the habit's name and measurement of completion, in addition to its streak
+///         It does not contain any logic for managing multiple habits or how they are completed.
+///     </para>
+/// </summary>
+public class Habit
 {
+
+    /// <summary>
+    ///     <para>
+    ///         A private string variable that represents the name of the habit.
+    ///     </para>
+    /// </summary>
     private string _name;
+    
+    /// <summary>
+    ///     <para>
+    ///         A private string variable that represents the description of the habit.
+    ///     </para>
+    /// </summary>
+    private string _description;
+    
+    /// <summary>
+    ///     <para>
+    ///         A private int variable that represents the current streak of the habit.
+    ///     </para>
+    /// </summary>
     private int _currentStreak;
     
+    /// <summary>
+    ///     <para>
+    ///         A private int variable that represents the longest streak of the habit.
+    ///     </para>
+    /// </summary>
+    private int _longestStreak;
+    
+    /// <summary>
+    ///     <para>
+    ///         A constructor that initializes a new instance of the Habit class.
+    ///     </para>
+    /// </summary>
     public Habit()
     {
         
     }
+
+    /// <summary>
+    ///     <para>
+    ///         Updates the current streak of the habit to the inputted new streak value.
+    ///         If the new streak is greater than the current longest streak, the longest streak is also updated.
+    ///     </para>
+    /// </summary>
+    /// <param name="newStreak">The new streak that the habit has reached.</param>
+    public void updateStreak(int newStreak)
+    {
+        
+    }
+
+    /// <summary>
+    ///     <para>
+    ///         Retrieves the current streak of the habit.
+    ///     </para>
+    /// </summary>
+    /// <returns>An integer value representing the current streak of the habit.</returns>
+    public int getCurrentStreak()
+    {
+        return 0;
+    }
+
+    /// <summary>
+    ///     <para>
+    ///         Retrieves the longest streak of the habit.
+    ///     </para>
+    /// </summary>
+    /// <returns>An integer value representing the longest streak of the habit.</returns>
+    public int getLongestStreak()
+    {
+        return 0;
+    }
+    
 }
--- HabitTracker/Habit/obj/Debug/net10.0/Habit.AssemblyInfo.cs
+++ HabitTracker/Habit/obj/Debug/net10.0/Habit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Habit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+051f2ae8a191f63d2bc846f4ecb633706e3f7cb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d03c3faa0a73334d47df27d818744d34871b6831")]
 [assembly: System.Reflection.AssemblyProductAttribute("Habit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Habit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
